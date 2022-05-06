806c7087f1d4f42a5786342c494a099faef8f2c4
NazariiB <92984941+NazariiB@users.noreply.github.com>
2022-05-06 19:11:02 +0300

    update MyToken

--- MyToken.sol
+++ MyToken.sol
@@ -15,41 +15,22 @@ contract MyToken is Context, ERC20, AccessControl {
 
 // you can't transfer over 100 tokens if you are an user
 // you can transfer a lot tokens if you are an admin
-    function transfer(address to, uint256 amount) public virtual override returns(bool) {
-        bool res = false;
-        if(hasRole(ADMIN, _msgSender())){
-            _transfer(_msgSender(), to, amount);
-            res = true;
-        }else if(hasRole(USER, _msgSender())){
-            require(amount < 100, "you can't transfer over 100 tokens");
-            _transfer(_msgSender(), to, amount);
-            res = true;
-        }else {
-            revert("you have no role");
-        }
-        return res;
+    function transfer(address to, uint256 amount) public override returns(bool) {
+        require((hasRole(USER, _msgSender()) && amount < 100) || hasRole(ADMIN, _msgSender()), "error");
+        _transfer(_msgSender(), to, amount);
+        return true;
     }
 
 // user can mint if user have more than 1000 tokens
 // admin can mint
-    function mint(address account, uint256 amount)public virtual {
-        if(hasRole(ADMIN, _msgSender())) {
-            _mint(account, amount);
-        }else if(hasRole(USER, _msgSender())) {
-            require(balanceOf(_msgSender()) > 1000, "you do not have enough tokens");
-            _mint(account, amount);
-        }else {
-            revert("you have no role");
-        }
+    function mint(address account, uint256 amount)public {
+        require(hasRole(ADMIN, _msgSender()) || (hasRole(USER, _msgSender()) && balanceOf(_msgSender()) > 1000), "error");
+        _mint(account, amount);
     }
 
 // only admin can burn
-    function burn(address account, uint256 amount) public virtual onlyRole(ADMIN) {
+    function burn(address account, uint256 amount) public onlyRole(ADMIN) {
         _burn(account, amount);
     }
 
-// only admin can add an account a role
-    function grantRole(bytes32 role, address account)public virtual override onlyRole(ADMIN){
-        _grantRole(role, account);
-    }
 }
